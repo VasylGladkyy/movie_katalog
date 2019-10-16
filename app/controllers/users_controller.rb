4c6639c5929a7af925f14cfd7516a81a0a46cd79
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-16 21:58:18 +0300

    Generate UsersController

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    #Todo
+  end
+
+  def show
+    #Todo
+  end
+
+  def edit
+    #Todo
+  end
+end
