eee55896638d16583cd54a210a6add1948365307
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-17 23:03:44 +0300

    Ovveride devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
   protect_from_forgery
 
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:warning] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 end
