0cec3acf0b94b23da927135449b9e07175149753
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-18 11:29:44 +0300

    Add users table to users index and add simple changes to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update, :destroy]
   root "movies#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
