cd02484168edf260787d1b8b44ca2049fce1c273
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-18 11:26:08 +0300

    Add method change_status and Overrided method active_for_auhentication on User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
 
   enum role: %i[admin customer]
+
+  def change_status
+    if active
+      update_attributes(active: false)
+    else
+      update_attributes(active: true)
+    end
+  end
+
+  def active_for_authentication?
+    super && active
+  end
 end
