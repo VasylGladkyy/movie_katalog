7ff0016b04f7447f0a9be726ef0641abfaf6025d
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-18 22:17:40 +0300

    Add factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.safe_email }
+    password { Faker::String.random(length: 8) }
+    nickname { Faker::Name.first_name }
+    role { "customer" }
+  end
+  
+  factory :admin, parent: :user do
+    role { "admin" }
+  end
+end
