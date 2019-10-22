78afcae7dc2de38a7e1711177153b1f66a2210b5
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-22 20:29:44 +0300

    Refactor method search and move search_form to layouts directory

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,12 @@ class Movie < ApplicationRecord
   validates :ganre, :release_date, :director, :actors, :plot, presence: true
   validates :imdbRating, presence: true, numericality: true
   validates :metascore, presence: true, numericality: { only_integer: true }
+
+  def self.search(title:)
+    if title
+      where('title LIKE ?', "%#{title}%")
+    else
+      all
+    end
+  end
 end
