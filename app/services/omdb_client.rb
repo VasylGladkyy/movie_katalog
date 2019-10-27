3bd307ad404c0500d316315244e0cde56949755b
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-10-27 15:44:39 +0200

    Fix problem with movie that don't have a poster

--- app/services/omdb_client.rb
+++ app/services/omdb_client.rb
@@ -8,7 +8,7 @@ class OmdbClient
   def by_id(id:)
     all(i: String(id))
   end
-  
+
   def by_title_all(title:)
     all(s: String(title))
   end
@@ -24,14 +24,16 @@ class OmdbClient
   end
 
   def check_search(responce:)
-    responce.key?('Search') ? arr_of_not_db_movies(array: responce['Search']) : responce
+    responce.key?('Search') ? valid_movies_array(array: responce['Search']) : responce
   end
 
   def check_responce(responce:)
     responce.key?('Error') ? nil : check_search(responce: responce)
   end
 
-  def arr_of_not_db_movies(array:)
-    array.delete_if { |h| Movie.exists?(imdb_id: h['imdbID']) || h['Type'] != 'movie' }
+  def valid_movies_array(array:)
+    array.delete_if do |movie|
+      Movie.exists?(imdb_id: movie['imdbID']) || movie['Type'] != 'movie' || movie['Poster'] == 'N/A'
+    end
   end
 end
